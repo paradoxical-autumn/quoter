1e75eb35a1ca17ce6d4c513d86369ed1bd76389c
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-03-18 13:15:23 +0000

    random code i needed for a bit

---
+++ src/unused_plugins/echo.py
@@ -0,0 +1,25 @@
+import lightbulb, hikari, miru, logging
+
+plugin = lightbulb.Plugin("echo")
+
+def load(bot: lightbulb.BotApp):
+    try:
+        bot.add_plugin(plugin)
+    except lightbulb.CommandAlreadyExists as err:
+        logging.error(err)
+
+def unload(bot: lightbulb.BotApp):
+    bot.remove_plugin(plugin)
+
+@plugin.listener(hikari.MessageCreateEvent)
+async def onMessage(event: hikari.MessageCreateEvent):
+    if event.is_human and event.content:
+        me = plugin.bot.get_me()
+        if str(event.author_id) == "730089700139991060":
+            if event.content.lower().startswith(f"{me.mention} echo"):
+                splitContent = event.content.split()
+                newMsg = ""
+                for i in range(2, len(splitContent)):
+                    newMsg += f"{splitContent[i]} "
+                
+                await event.message.respond(newMsg)
