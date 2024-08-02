1901289ab493014726b5b506efecb966c3870dab
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-08-02 14:19:13 +0100

    remove unused class

--- src/main.py
+++ src/main.py
@@ -74,39 +74,6 @@ cooldownList = ["HEY!", "Wait up!", ">:C", "a.", ":ice_cube:", ":fire: :fire: :f
 permissionErrorFlavourText = ["nope.", "I don't think I can do that...", ":nerd:", ":warning:", "oi", "The law requires I answer no.", "`permissionError`", "nuh uh."]
 errorFlavourText = ["i didn't touch nothing!", "deleting system 32...", "umm uhh", "im so silly!", "oops...", "i think i dropped something.", "[500] internal server error"]
 
-class bugReportModal(miru.Modal):
-    info = miru.TextInput(label="What we're you doing when the error happened?", style=hikari.TextInputStyle.PARAGRAPH, required=True, placeholder="Something like: \"i was just using the bot ;-;\"")
-    contact = miru.TextInput(label="Are we allowed to contact you?", style=hikari.TextInputStyle.SHORT, required=True, placeholder="yeah")
-
-    async def callback(self, ctx: miru.ModalContext) -> None:
-        embedData = {}
-
-        if ctx.author.avatar_url:
-            aviUrl = str(ctx.author.avatar_url)
-        else:
-            aviUrl = "https://cdn.discordapp.com/embed/avatars/0.png"
-        
-        embedData["embeds"] = [
-            {
-                "title": f"New bug report",
-                "description": f"from {ctx.author.username}",
-                "fields": [
-                    {
-                        "name": "explanation",
-                        "value": self.info.value
-                    },
-                    {
-                        "name": "contact allowed?",
-                        "value": self.contact.value
-                    }
-                ]
-            }
-        ]
-
-        requests.post(os.environ["ERROR_WEBHOOK"], json=embedData)
-
-        await ctx.respond("sent, thanks for the report!")
-
 class bugReportView(miru.View):
     pass
 
